5b4678534ac0992a9f1f60eaa8f6d9e89c05c5f0
Mamitina Jean Felix Ratojoniaina <maratojo@c1r12s4.42antananarivo.mg>
2026-02-14 13:46:36 +0300

    ft_printf modified

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,37 @@
 /*   By: maratojo <maratojo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 09:54:35 by maratojo          #+#    #+#             */
-/*   Updated: 2026/02/14 11:07:02 by maratojo         ###   ########.fr       */
+/*   Updated: 2026/02/14 13:42:38 by maratojo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-int ft_printf(const char *, ...)
+#include <stdarg.h>
+
+static int	handle_conversion(char c)
+{
+	int	count;
+	va_list arg;
+
+	count = 0;
+	if(c = 'c')
+		count += ft_putchar(va_arg(arg, char));
+	else if (c = 's')
+		count += ft_putstr(va_arg(arg, char));
+	else if (c = 'p')
+		count += ft_putptr(va_arg(arg, int));
+	else if ((c = 'd') || (c = 'i'))
+		count += ft_putnbr(va_arg(arg, int));
+	else if (c = 'u')
+		count += ft_putnbr_unsigned(va_arg(arg, unsigned int));
+	else if (c = 'x')
+		count += ft_puthexa(va_arg(arg, int), "0123456789abcdef");
+	else if (c = 'X')
+		count += ft_puthexa(va_arg(arg, int), "0123456789ABCDEF");
+	return (count);
+}
+
+int ft_printf(const char *format, ...)
 {
 
 }
